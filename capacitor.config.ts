c43ca93428e2d098db97e56bd8a6f1201bc52990
Andres-DLT <A.DLT.G@OUTLOOK.com>
2026-09-06 10:46:11 -0600

    Cambios para migrar de Angular a Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.phoneDevelopers.app',
-  appName: 'phoneDevelopers',
-  webDir: 'dist/phoneDevelopers/browser',
+  appId: 'com.example.app',
+  appName: 'marketplace',
+  webDir: 'dist'
 };
 
 export default config;

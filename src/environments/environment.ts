ccea30c5d7eab01d2988cd28a1f2d7f0899fd5a7
Andres-DLT <A.DLT.G@OUTLOOK.com>
2026-09-19 13:15:39 -0600

    fix(ci): track src/environments/environment.ts con config publica smartfoodie-dda27 para build en CI

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAPuh9YGxn4KA7HPy2yoRuomPuOtDXIfY8',
+    authDomain: 'smartfoodie-dda27.firebaseapp.com',
+    projectId: 'smartfoodie-dda27',
+    storageBucket: 'smartfoodie-dda27.firebasestorage.app',
+    messagingSenderId: '626578601404',
+    appId: '1:626578601404:web:1de784bee9aac0581e27b9',
+    measurementId: "G-8KYNQVMBBF",
+  },
+};
